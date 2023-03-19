dc259c34cdc6e8a42471bd882b3630ec57daef50
jafar <jafar.fahimi12@gmail.com>
2023-03-19 22:21:32 +0430

    _app wrapped with ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,26 @@ import { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/style.css";
 import Head from "next/head";
+import { ColorModeContext, useMode } from "../styles/theme";
+// CssBaseline reset css
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  const [theme, colorMode] = useMode();
   return (
     <>
       <Head>
         <title>Next Admin Dashboard Design</title>
         <link rel="icon" href="chart-logo.png" />
       </Head>
-      <Component {...pageProps} />
+
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <main className="content">
+            <Component {...pageProps} />
+          </main>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </>
   );
 };

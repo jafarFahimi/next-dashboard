4b4264ab3b31488b53845086e1a76a63cef8cbef
jafar <jafar.fahimi12@gmail.com>
2023-03-19 20:51:19 +0430

    style.css coded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import "../styles/style.css";
 import Head from "next/head";
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <>
       <Head>
-        <title> Twitter Design</title>
-        <link rel="icon" href="logo.svg" />
+        <title>Next Admin Dashboard Design</title>
+        <link rel="icon" href="chart-logo.png" />
       </Head>
       <Component {...pageProps} />
     </>

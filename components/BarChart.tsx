4506691f2a4ff5e3bb5c55bf9160afe02e94e8ec
jafar <jafar.fahimi12@gmail.com>
2023-03-23 22:07:37 +0430

    BarChart.tsx theme tooltip fixed

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -16,6 +16,11 @@ const BarChart: NextPage<{ isDashboard: boolean }> = ({
     <ResponsiveBar
       data={mockBarData}
       theme={{
+        tooltip: {
+          basic: {
+            color: colors.primary[300],
+          },
+        },
         axis: {
           domain: {
             line: {
@@ -94,7 +99,7 @@ const BarChart: NextPage<{ isDashboard: boolean }> = ({
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "country",
+        legend: isDashboard ? undefined : "country",
         legendPosition: "middle",
         legendOffset: 32,
       }}
@@ -102,7 +107,7 @@ const BarChart: NextPage<{ isDashboard: boolean }> = ({
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "food",
+        legend: isDashboard ? undefined : "food",
         legendPosition: "middle",
         legendOffset: -40,
       }}
@@ -143,125 +148,6 @@ const BarChart: NextPage<{ isDashboard: boolean }> = ({
       }
     />
   );
-  //   <ResponsiveBar
-  //     data={mockBarData}
-  //     theme={{
-  //       axis: {
-  //         domain: {
-  //           line: {
-  //             stroke: colors.grey[100],
-  //           },
-  //         },
-  //         legend: {
-  //           text: {
-  //             fill: colors.grey[100],
-  //           },
-  //         },
-  //         ticks: {
-  //           line: {
-  //             stroke: colors.grey[100],
-  //             strokeWidth: 1,
-  //           },
-  //           text: {
-  //             fill: colors.grey[100],
-  //           },
-  //         },
-  //       },
-  //       legends: {
-  //         text: {
-  //           fill: colors.grey[100],
-  //         },
-  //       },
-  //     }}
-  //     keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-  //     indexBy="country"
-  //     margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-  //     padding={0.3}
-  //     valueScale={{ type: "linear" }}
-  //     indexScale={{ type: "band", round: true }}
-  //     // colors={{ scheme: "nivo" } as unknown as Colors}
-  //     defs={[
-  //       {
-  //         id: "dots",
-  //         type: "patternDots",
-  //         background: "inherit",
-  //         color: "#38bcb2",
-  //         size: 4,
-  //         padding: 1,
-  //         stagger: true,
-  //       },
-  //       {
-  //         id: "lines",
-  //         type: "patternLines",
-  //         background: "inherit",
-  //         color: "#eed312",
-  //         rotation: -45,
-  //         lineWidth: 6,
-  //         spacing: 10,
-  //       },
-  //     ]}
-  //     // borderColor={{
-  //     //   from: "color",
-  //     //   modifiers: [["darker", "1.6"]],
-  //     // }}
-  //     // axisTop={null}
-  //     // axisRight={null}
-  //     axisBottom={{
-  //       tickSize: 5,
-  //       tickPadding: 5,
-  //       tickRotation: 0,
-  //       legend: isDashboard ? undefined : "country", // changed
-  //       legendPosition: "middle",
-  //       legendOffset: 32,
-  //     }}
-  //     axisLeft={{
-  //       tickSize: 5,
-  //       tickPadding: 5,
-  //       tickRotation: 0,
-  //       legend: isDashboard ? undefined : "food", // changed
-  //       legendPosition: "middle",
-  //       legendOffset: -40,
-  //     }}
-  //     enableLabel={false}
-  //     labelSkipWidth={12}
-  //     labelSkipHeight={12}
-  //     // labelTextColor={
-  //     //   {
-  //     //     from: "color",
-  //     //     modifiers: [["darker", 1.6]],
-  //     //   } as unknown as string
-  //     // }
-  //     labelTextColor="#737373"
-  //     legends={[
-  //       {
-  //         dataFrom: "keys",
-  //         anchor: "bottom-right",
-  //         direction: "column",
-  //         justify: false,
-  //         translateX: 120,
-  //         translateY: 0,
-  //         itemsSpacing: 2,
-  //         itemWidth: 100,
-  //         itemHeight: 20,
-  //         itemDirection: "left-to-right",
-  //         itemOpacity: 0.85,
-  //         symbolSize: 20,
-  //         effects: [
-  //           {
-  //             on: "hover",
-  //             style: {
-  //               itemOpacity: 1,
-  //             },
-  //           },
-  //         ],
-  //       },
-  //     ]}
-  //     role="application"
-  //     barAriaLabel={(e: any) =>
-  //       e.id + ": " + e.formattedValue + " in country: " + e.indexValue
-  //     }
-  //   />
-  // );
 };
 
 export default BarChart;

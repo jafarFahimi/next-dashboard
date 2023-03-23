d5b570ca3fb027dcdd632e11e60cef8daea22fcd
jafar <jafar.fahimi12@gmail.com>
2023-03-23 22:24:18 +0430

    pages/pie.tsx completed

--- pages/pie.tsx
+++ pages/pie.tsx
@@ -1,8 +1,18 @@
+import { Box } from "@mui/material";
+import PieChart from "components/PieChart";
+import Header from "components/Header";
 import { NextPage } from "next";
 import React from "react";
 
-const Pie: NextPage = () => {
-  return <div>Pie</div>;
+const PiePage: NextPage = () => {
+  return (
+    <Box m="20px">
+      <Header title="Pie Chart" subtitle="simple Pie chart from nivo.rocks" />
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  );
 };
 
-export default Pie;
+export default PiePage;

6fa1f6cf3f4f2468e3ee95133a48bae321a47c21
jafar <jafar.fahimi12@gmail.com>
2023-03-23 21:53:10 +0430

    BarChart.tsx head type fixed

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -5,7 +5,10 @@ import { tokens } from "styles/theme";
 import { Colors } from "@nivo/core";
 import { NextPage } from "next";
 
-const BarChart: NextPage<any> = ({ isDashboard = false }) => {
+// is used in dashboard & another page
+const BarChart: NextPage<{ isDashboard: boolean }> = ({
+  isDashboard = false,
+}) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
@@ -30,7 +33,7 @@ const BarChart: NextPage<any> = ({ isDashboard = false }) => {
               strokeWidth: 1,
             },
             text: {
-              fill: colors.grey[100],
+              fill: colors.grey[400],
             },
           },
         },
@@ -46,7 +49,7 @@ const BarChart: NextPage<any> = ({ isDashboard = false }) => {
       padding={0.3}
       valueScale={{ type: "linear" }}
       indexScale={{ type: "band", round: true }}
-      colors={{ scheme: "nivo" } as unknown as Colors}
+      // colors={{ scheme: "nivo" }}
       defs={[
         {
           id: "dots",
@@ -67,15 +70,31 @@ const BarChart: NextPage<any> = ({ isDashboard = false }) => {
           spacing: 10,
         },
       ]}
+      fill={[
+        {
+          match: {
+            id: "fries",
+          },
+          id: "dots",
+        },
+        {
+          match: {
+            id: "sandwich",
+          },
+          id: "lines",
+        },
+      ]}
       // borderColor={{
       //   from: "color",
-      //   modifiers: [["darker", "1.6"]],
+      //   modifiers: [["darker", 1.6]],
       // }}
+      // axisTop={null}
+      // axisRight={null}
       axisBottom={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "country", // changed
+        legend: "country",
         legendPosition: "middle",
         legendOffset: 32,
       }}
@@ -83,19 +102,16 @@ const BarChart: NextPage<any> = ({ isDashboard = false }) => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "food", // changed
+        legend: "food",
         legendPosition: "middle",
         legendOffset: -40,
       }}
-      enableLabel={false}
       labelSkipWidth={12}
       labelSkipHeight={12}
-      labelTextColor={
-        {
-          from: "color",
-          modifiers: [["darker", 1.6]],
-        } as unknown as string
-      }
+      // labelTextColor={{
+      //   from: "color",
+      //   modifiers: [["darker", 1.6]],
+      // }}
       legends={[
         {
           dataFrom: "keys",
@@ -121,11 +137,131 @@ const BarChart: NextPage<any> = ({ isDashboard = false }) => {
         },
       ]}
       role="application"
+      ariaLabel="Nivo bar chart demo"
       barAriaLabel={(e: any) =>
         e.id + ": " + e.formattedValue + " in country: " + e.indexValue
       }
     />
   );
+  //   <ResponsiveBar
+  //     data={mockBarData}
+  //     theme={{
+  //       axis: {
+  //         domain: {
+  //           line: {
+  //             stroke: colors.grey[100],
+  //           },
+  //         },
+  //         legend: {
+  //           text: {
+  //             fill: colors.grey[100],
+  //           },
+  //         },
+  //         ticks: {
+  //           line: {
+  //             stroke: colors.grey[100],
+  //             strokeWidth: 1,
+  //           },
+  //           text: {
+  //             fill: colors.grey[100],
+  //           },
+  //         },
+  //       },
+  //       legends: {
+  //         text: {
+  //           fill: colors.grey[100],
+  //         },
+  //       },
+  //     }}
+  //     keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
+  //     indexBy="country"
+  //     margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+  //     padding={0.3}
+  //     valueScale={{ type: "linear" }}
+  //     indexScale={{ type: "band", round: true }}
+  //     // colors={{ scheme: "nivo" } as unknown as Colors}
+  //     defs={[
+  //       {
+  //         id: "dots",
+  //         type: "patternDots",
+  //         background: "inherit",
+  //         color: "#38bcb2",
+  //         size: 4,
+  //         padding: 1,
+  //         stagger: true,
+  //       },
+  //       {
+  //         id: "lines",
+  //         type: "patternLines",
+  //         background: "inherit",
+  //         color: "#eed312",
+  //         rotation: -45,
+  //         lineWidth: 6,
+  //         spacing: 10,
+  //       },
+  //     ]}
+  //     // borderColor={{
+  //     //   from: "color",
+  //     //   modifiers: [["darker", "1.6"]],
+  //     // }}
+  //     // axisTop={null}
+  //     // axisRight={null}
+  //     axisBottom={{
+  //       tickSize: 5,
+  //       tickPadding: 5,
+  //       tickRotation: 0,
+  //       legend: isDashboard ? undefined : "country", // changed
+  //       legendPosition: "middle",
+  //       legendOffset: 32,
+  //     }}
+  //     axisLeft={{
+  //       tickSize: 5,
+  //       tickPadding: 5,
+  //       tickRotation: 0,
+  //       legend: isDashboard ? undefined : "food", // changed
+  //       legendPosition: "middle",
+  //       legendOffset: -40,
+  //     }}
+  //     enableLabel={false}
+  //     labelSkipWidth={12}
+  //     labelSkipHeight={12}
+  //     // labelTextColor={
+  //     //   {
+  //     //     from: "color",
+  //     //     modifiers: [["darker", 1.6]],
+  //     //   } as unknown as string
+  //     // }
+  //     labelTextColor="#737373"
+  //     legends={[
+  //       {
+  //         dataFrom: "keys",
+  //         anchor: "bottom-right",
+  //         direction: "column",
+  //         justify: false,
+  //         translateX: 120,
+  //         translateY: 0,
+  //         itemsSpacing: 2,
+  //         itemWidth: 100,
+  //         itemHeight: 20,
+  //         itemDirection: "left-to-right",
+  //         itemOpacity: 0.85,
+  //         symbolSize: 20,
+  //         effects: [
+  //           {
+  //             on: "hover",
+  //             style: {
+  //               itemOpacity: 1,
+  //             },
+  //           },
+  //         ],
+  //       },
+  //     ]}
+  //     role="application"
+  //     barAriaLabel={(e: any) =>
+  //       e.id + ": " + e.formattedValue + " in country: " + e.indexValue
+  //     }
+  //   />
+  // );
 };
 
 export default BarChart;

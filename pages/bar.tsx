47727d6e6e925c36cb15ce17dd244c4f26abc5fe
jafar <jafar.fahimi12@gmail.com>
2023-03-23 21:53:18 +0430

    bar.tsx coded

--- pages/bar.tsx
+++ pages/bar.tsx
@@ -1,8 +1,18 @@
+import { Box } from "@mui/material";
+import BarChart from "components/BarChart";
+import Header from "components/Header";
 import { NextPage } from "next";
 import React from "react";
 
-const Bar: NextPage = () => {
-  return <div>Bar</div>;
+const BarPage: NextPage = () => {
+  return (
+    <Box m="20px">
+      <Header title="Bar Chart" subtitle="simple Bar chart from nivo.rocks" />
+      <Box height="75vh">
+        <BarChart isDashboard={false} />
+      </Box>
+    </Box>
+  );
 };
 
-export default Bar;
+export default BarPage;
